2a01607f91fba183417455b5724975b2c9d37af9
Hasibullah Aman <hasib.aman7@gmail.com>
2023-10-07 10:24:20 +0430

    init coco.txt and modified predict.py

---
+++ predict.py
@@ -0,0 +1,34 @@
+
+
+# ------------------------- Import Library
+
+import cv2
+import numpy as np  # Importing NumPy library for numerical operations
+from ultralytics import YOLO  # Importing YOLO from ultralytics package
+import pandas as pd  # Importing Pandas library for data manipulation and analysis
+import time  # Importing Time module for timing operations
+
+# ------------------------------ Import Model
+
+# in this line we import the model we trained in Car Object Detection file
+
+model=YOLO('./best.pt')
+
+# fining the mouse position
+def RGB(event, x, y, flags, param):
+    if event == cv2.EVENT_MOUSEMOVE :
+        colorsBGR = [x, y]
+        print(colorsBGR)
+
+
+# cv2.namedWindow('combined_video1')
+# cv2.setMouseCallback('combined_video1', RGB)
+# cv2.namedWindow('combined_video2')
+# cv2.setMouseCallback('combined_video2', RGB)
+
+# ----------------------------- Read Class
+my_file = open("coco.txt", "r")
+data = my_file.read()
+class_list = data.split("\n")
+#print(class_list)
+# count=0
