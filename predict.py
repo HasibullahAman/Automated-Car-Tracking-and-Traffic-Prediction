8a1c12385ed733e4ea0298c838cf24a008f28a66
Hasibullah Aman <hasib.aman7@gmail.com>
2023-10-07 10:56:01 +0430

    ignore video and start predict.py file

--- predict.py
+++ predict.py
@@ -1,5 +1,3 @@
-
-
 # ------------------------- Import Library
 
 import cv2
@@ -12,11 +10,12 @@ import time  # Importing Time module for timing operations
 
 # in this line we import the model we trained in Car Object Detection file
 
-model=YOLO('./best.pt')
+model = YOLO('./best.pt')
+
 
 # fining the mouse position
 def RGB(event, x, y, flags, param):
-    if event == cv2.EVENT_MOUSEMOVE :
+    if event == cv2.EVENT_MOUSEMOVE:
         colorsBGR = [x, y]
         print(colorsBGR)
 
@@ -30,5 +29,12 @@ def RGB(event, x, y, flags, param):
 my_file = open("coco.txt", "r")
 data = my_file.read()
 class_list = data.split("\n")
-#print(class_list)
+print(class_list)
 # count=0
+
+
+# --------------------------------- Read video
+isopen = True
+while isopen:
+    cap1 = cv2.VideoCapture('download.mp4')
+
