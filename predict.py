a84b715dde9b5694f78f390d1b819e49caa11d14
Hasibullah Aman <hasib.aman7@gmail.com>
2023-10-08 17:06:10 +0430

    update and add rectangle line in predict file

--- predict.py
+++ predict.py
@@ -24,9 +24,8 @@ cap = cv2.VideoCapture('vidyolov8.mp4')
 my_file = open("coco.txt", "r")
 data = my_file.read()
 class_list = data.split("\n")
-print(class_list)
 
-
+# ---------------------------------- Detect and assign rounding box + class name
 count = 0
 while True:
 
@@ -38,7 +37,24 @@ while True:
         continue
     frame = cv2.resize(frame, (1020, 500))
 
-    results = model.predict(frame, show=True)
+    results = model.predict(frame)
+    # print(results)
+    a = results[0].boxes.xyxy
+    px = pd.DataFrame(a).astype('float')
+    print(px)
+    for index,row in px.iterrows():
+        x1 = int(row[0])
+        y1 = int(row[1])
+        x2 = int(row[2])
+        y2 = int(row[3])
+
+
+
+
+
+
+
+
 
     cv2.imshow("RGB", frame)
     if cv2.waitKey(1) & 0xFF == 27:
