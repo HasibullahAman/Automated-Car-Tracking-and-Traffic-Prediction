45d747dd97d74bf668d82204b21947bde51022ba
Hasibullah Aman <hasib.aman7@gmail.com>
2023-10-09 18:30:29 +0430

    commit all monday code

--- predict.py
+++ predict.py
@@ -3,12 +3,16 @@ import cv2
 import pandas as pd
 import numpy as np
 from ultralytics import YOLO
+from tracker import * # this module used for tracking
 
 # ---------------------------- import model
 model = YOLO('best.pt')
 
 
 # -------------------------- add mouse option for finding the line x and y coordinate
+area = [(283, 281), (203, 307), (549, 343), (562, 295)]
+tracker = Tracker()
+area_set = set()
 def RGB(event, x, y, flags, param):
     if event == cv2.EVENT_MOUSEMOVE:
         colorsBGR = [x, y]
@@ -19,7 +23,7 @@ cv2.namedWindow('RGB')
 cv2.setMouseCallback('RGB', RGB)
 
 # -----------------------------------import video file
-cap = cv2.VideoCapture('vidyolov8.mp4')
+cap = cv2.VideoCapture('FirstVideo.mp4')
 # ----------------------------------- Read the class file
 my_file = open("coco.txt", "r")
 data = my_file.read()
@@ -38,12 +42,12 @@ while True:
     count += 1
     if count % 3 != 0:
         continue
-    frame = cv2.resize(frame, (1020, 500))
-
-    results = model.predict(frame)
+    frame = cv2.resize(frame, (1020, 500)) # set the size of frame
+    results = model.predict(frame) # make prediction
     # ---------------------------- draw a lin and display the class name
     a = results[0].boxes.data
     px = pd.DataFrame(a).astype('float')
+    list = []
     for index, row in px.iterrows():
         x1 = int(row[0])
         y1 = int(row[1])
@@ -51,12 +55,27 @@ while True:
         y2 = int(row[3])
         d = int(row[5])
         c = class_list[d]
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 1)
-        cv2.putText(frame, str(c), (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 1)
-
+        if 'car' or 'truck' or 'bike' or 'bus' in c:
+            list.append([x1, y1, x2,  y2])
+    bbox_id = tracker.update(list)
+    for bbox in bbox_id:
+        x3, y3, x4, y4, id= bbox
+        cx = int(x3+x4) // 2
+        cy = int(y3+y4) // 2
+        results = cv2.pointPolygonTest(np.array(area, np.int32), (cx, cy), False)
+        if results >= 0:
+            # show the center of object
+            cv2.circle(frame, (cx, cy), 4, (0, 0, 255), -1)
+            # Draw a line when detect
+            # cv2.rectangle(frame, (x3, y3), (x4, y4), (0, 0, 255), 1)
+            # show the object ID when detect
+            # cv2.putText(frame, str(id), (x3, y3), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 1)
+            area_set.add(id)
+    cv2.polylines(frame, [np.array(area, np.int32)], True, (255, 255, 0), 1) # draw a box which we count the object in
+    count = len(area_set)
+    cv2.putText(frame, str(count), (50, 50), cv2.FONT_HERSHEY_PLAIN, 0.9, (255, 255, 255), 1)  # show the count of car in the frame
     cv2.imshow("RGB", frame)
     if cv2.waitKey(1) & 0xFF == 27:
         break
-print("My name is hasib")
 cap.release()
 cv2.destroyAllWindows()
